5e46c2b253641ad6a660b36b861d16cf0a0d25cd
Cary Woods <cary@indybiosystems.com>
2024-02-23 12:04:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn as 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
